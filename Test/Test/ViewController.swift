a6d3712be982c8033e1a819b41a744fcccf34e07
rp-ios <developmentios1@gmail.com>
2021-07-27 07:56:57 +0300

    added outlet

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -8,12 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet private weak var label: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
 
